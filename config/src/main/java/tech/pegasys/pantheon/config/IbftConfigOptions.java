08b79fe36ddeaa7ec147e8f4bea8e8be9dc3f7c1
Jason Frame <jasonwframe@gmail.com>
2019-02-04 11:35:29 +1000

    comment on why default gossipped history limit was chosen

--- config/src/main/java/tech/pegasys/pantheon/config/IbftConfigOptions.java
+++ config/src/main/java/tech/pegasys/pantheon/config/IbftConfigOptions.java
@@ -21,6 +21,8 @@ public class IbftConfigOptions {
   private static final long DEFAULT_EPOCH_LENGTH = 30_000;
   private static final int DEFAULT_BLOCK_PERIOD_SECONDS = 1;
   private static final int DEFAULT_ROUND_EXPIRY_SECONDS = 1;
+  // In a healthy network this can be very small. This default limit will allow for suitable
+  // protection for on a typical 20 node validator network with multiple rounds
   private static final int DEFAULT_GOSSIPED_HISTORY_LIMIT = 1000;
   private static final int DEFAULT_MESSAGE_QUEUE_LIMIT = 1000;
 
